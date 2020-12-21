ad25960cae581b7a25b1915968b14da8c9628c43
Bricktech2000 <bricktech2000@gmail.com>
2020-12-21 12:42:10 -0500

    chained input video stream to squared canvas

--- sensoryAddition.js
+++ sensoryAddition.js
@@ -1,16 +1,33 @@
 window.SensoryAddition = {
-    cameraIn: async function(){
+    cameraIn: async function(canvas){
         //https://www.damirscorner.com/blog/posts/20170317-RenderCapturedVideoToFullPageCanvas.html
         var video = document.createElement('video');
         document.body.appendChild(video);
+        var ctx = canvas.getContext('2d');
+
         if(navigator.mediaDevices.getUserMedia){
             var stream = await navigator.mediaDevices.getUserMedia({
                 audio: false,
-                //video: { facingMode: { ideal: 'environment' } },
-                video: true,
+                video: { facingMode: { ideal: 'environment' } },
             });
             video.srcObject = stream;
             video.play();
         }else throw new Error('getUserMedia not supported');
+        function draw(){
+            aspectRatio = video.videoWidth / video.videoHeight;
+            videoWidth = canvas.height * aspectRatio;
+            videoHeight = canvas.width / aspectRatio;
+            videoWidthCut = Math.max(0, videoWidth - canvas.width);
+            videoHeightCut = Math.max(0, videoHeight - canvas.height);
+            ctx.drawImage(
+                video,
+                0 - videoWidthCut / 2,
+                0 - videoHeightCut / 2,
+                canvas.width + videoWidthCut,
+                canvas.height + videoHeightCut
+            );
+            requestAnimationFrame(draw);
+        }
+        draw();
     }
 }
