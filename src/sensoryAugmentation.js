3f20d0a39a019257465b1f1d9f8aa954865ecff9
Bricktech <bricktech2000@gmail.com>
2023-01-12 14:56:28 -0500

    updated all `var` to `let`

--- src/sensoryAugmentation.js
+++ src/sensoryAugmentation.js
@@ -3,14 +3,14 @@ window.SensoryAugmentation = {
     this.resolution = resolution;
     this.size = Math.pow(2, resolution);
     //https://www.damirscorner.com/blog/posts/20170317-RenderCapturedVideoToFullPageCanvas.html
-    var video = document.createElement('video');
+    let video = document.createElement('video');
     document.body.appendChild(video);
-    var canvas = document.createElement('canvas');
+    let canvas = document.createElement('canvas');
     canvas.width = canvas.height = this.size;
-    var ctx = canvas.getContext('2d');
+    let ctx = canvas.getContext('2d');
 
     if (navigator.mediaDevices.getUserMedia) {
-      var stream = await navigator.mediaDevices.getUserMedia({
+      let stream = await navigator.mediaDevices.getUserMedia({
         audio: false,
         //video: true,
         //the following crashes both Chrome and Chromium Edge...
@@ -42,21 +42,21 @@ window.SensoryAugmentation = {
     return ctx;
   },
   toData: async function (ctx) {
-    var canvas = ctx.canvas;
-    var self = this.toData;
+    let canvas = ctx.canvas;
+    let self = this.toData;
     self.data = new Array(this.size * this.size);
 
-    var canvas2 = document.createElement('canvas');
+    let canvas2 = document.createElement('canvas');
     canvas2.width = canvas2.height = this.size;
-    var ctx2 = canvas2.getContext('2d');
+    let ctx2 = canvas2.getContext('2d');
 
     return new Promise((resolve, reject) => {
       canvas.addEventListener('draw', () => {
         //https://en.wikipedia.org/wiki/Hilbert_curve
         function xy2d(n, x, y) {
-          var xa = [x],
+          let xa = [x],
             ya = [y];
-          var rx,
+          let rx,
             ry,
             s,
             d = 0;
@@ -75,7 +75,7 @@ window.SensoryAugmentation = {
                 ya[0] = n - 1 - ya[0];
               }
               //Swap x and y
-              var t = xa[0];
+              let t = xa[0];
               xa[0] = ya[0];
               ya[0] = t;
             }
@@ -83,36 +83,36 @@ window.SensoryAugmentation = {
         }
         //https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Pixel_manipulation_with_canvas
         function getColorIndicesForCoord(x, y, width) {
-          var red = y * (width * 4) + x * 4;
+          let red = y * (width * 4) + x * 4;
           return [red, red + 1, red + 2, red + 3];
         }
 
         const pixelDarkener = 5; //a constant to darken the image, reducing noise
-        var imageData = ctx.getImageData(
+        let imageData = ctx.getImageData(
           0,
           0,
           canvas.width,
           canvas.height
         ).data;
-        var average =
+        let average =
           ((imageData.reduce((a, v) => a + v, 0) / imageData.length -
             255 / 4) /*alpha*/ /
             3) *
           pixelDarkener;
-        var modData = new Uint8ClampedArray(this.size * this.size * 4);
+        let modData = new Uint8ClampedArray(this.size * this.size * 4);
 
-        for (var x = 0; x < canvas.width; x++) {
-          for (var y = 0; y < canvas.height; y++) {
-            var colorIndices = getColorIndicesForCoord(x, y, canvas.width);
-            var pixelData = [
+        for (let x = 0; x < canvas.width; x++) {
+          for (let y = 0; y < canvas.height; y++) {
+            let colorIndices = getColorIndicesForCoord(x, y, canvas.width);
+            let pixelData = [
               imageData[colorIndices[0]],
               imageData[colorIndices[1]],
               imageData[colorIndices[2]],
             ];
-            var pixelAverage = Math.floor(
+            let pixelAverage = Math.floor(
               pixelData.reduce((a, b) => a + b, 0) / 3
             );
-            var delta =
+            let delta =
               Math.abs(pixelAverage - average) * /*noise multiplier*/ 2 -
               /*noise floor*/ 128;
             modData[colorIndices[0]] = delta;
@@ -124,17 +124,17 @@ window.SensoryAugmentation = {
         }
 
         //https://stackoverflow.com/questions/22823752/creating-image-from-array-in-javascript-and-html5
-        var data = ctx2.createImageData(this.size, this.size);
+        let data = ctx2.createImageData(this.size, this.size);
         data.data.set(modData);
         ctx2.putImageData(data, 0, 0);
         resolve([self.data, ctx2]);
         //console.log(Math.max.apply(null, data));
         //console.log(data);
-        /*var d = [];
-                var i = 0;
-                for(var x = 0; x < this.size; x++){
+        /*let d = [];
+                let i = 0;
+                for(let x = 0; x < this.size; x++){
                     d[x] = [];
-                    for(var y = 0; y < this.size; y++){
+                    for(let y = 0; y < this.size; y++){
                         d[x][y] = xy2d(this.size, x, y);
                         i++;
                     }
@@ -146,16 +146,16 @@ window.SensoryAugmentation = {
   },
   soundOut: function (data, min, max) {
     //https://stackoverflow.com/questions/43386277/how-to-control-the-sound-volume-of-audio-buffer-audiocontext
-    var ctx = new (window.AudioContext || window.webkitAudioContext)();
-    var gainNodes = [];
-    var outputGain = ctx.createGain();
-    //var i = 0;
+    let ctx = new (window.AudioContext || window.webkitAudioContext)();
+    let gainNodes = [];
+    let outputGain = ctx.createGain();
+    //let i = 0;
     //console.log(data.length);
-    for (var i = 0; i < data.length; i++) {
-      var gain = ctx.createGain();
+    for (let i = 0; i < data.length; i++) {
+      let gain = ctx.createGain();
       gain.gain.value = 0;
       //gain.gain.exponentialRampToValueAtTime(1, ctx.currentTime + Math.random());
-      var osc = ctx.createOscillator();
+      let osc = ctx.createOscillator();
       osc.type = 'sine';
       osc.frequency.value =
         min * Math.pow(max / min, i / (this.size * this.size));
@@ -171,7 +171,7 @@ window.SensoryAugmentation = {
 
     function update() {
       //https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Advanced_techniques
-      for (var i = 0; i < data.length; i++) {
+      for (let i = 0; i < data.length; i++) {
         //if(i % 8 != 0) continue;
         gainNodes[i].gain.linearRampToValueAtTime(
           Math.max(0, data[i]),
